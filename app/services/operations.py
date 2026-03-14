ef647f3dc8c8035e770ece8139eeb92dad7685ac
Idrenal <dp310793pps@gmail.com>
2026-03-14 14:54:59 +0300

    refactor add: Dependency Injection

--- app/services/operations.py
+++ app/services/operations.py
@@ -1,49 +1,44 @@
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app.schemas import OperationRequest
 from app.repository import wallets as wallets_reposytory
 
-def add_income(operation: OperationRequest):
-    db = SessionLocal()
-    try:
-        if not wallets_reposytory.is_wallet_exist(db, operation.wallet_name):
-            raise HTTPException(
-                status_code=404,
-                detail=f"Wallet '{operation.wallet_name}' not found")
 
-        wallet = wallets_reposytory.add_income(db, operation.wallet_name, operation.amount)
-        db.commit()
-        return {
-            "message": "Income added",
-            "wallet": operation.wallet_name,
-            "amount": operation.amount,
-            "description": operation.description,
-            "new_balance": wallet.balance
-        }
-    finally:
-        db.close()
+def add_income(db: Session, operation: OperationRequest):
+    if not wallets_reposytory.is_wallet_exist(db, operation.wallet_name):
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{operation.wallet_name}' not found")
 
-def add_expense(operation: OperationRequest):
-    db = SessionLocal()
-    try:
-        if not wallets_reposytory.is_wallet_exist(db, operation.wallet_name):
-            raise HTTPException(status_code=404, detail=f"Wallet '{operation.wallet_name}' not found")
-        wallet = wallets_reposytory.get_wallet_balance_by_name(db, operation.wallet_name)
-        if wallet.balance < operation.amount:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Insufficient funds. Available: {wallet.balance}"
-            )
+    wallet = wallets_reposytory.add_income(db, operation.wallet_name, operation.amount)
+    db.commit()
+    return {
+        "message": "Income added",
+        "wallet": operation.wallet_name,
+        "amount": operation.amount,
+        "description": operation.description,
+        "new_balance": wallet.balance
+    }
 
-        wallet = wallets_reposytory.add_expense(db, operation.wallet_name, operation.amount)
-        db.commit()
-        return {
-            "message": "Expense added",
-            "wallet": operation.wallet_name,
-            "amount": operation.amount,
-            "description": operation.description,
-            "new_balance": wallet.balance
-        }
-    finally:
-        db.close()
+
+def add_expense(db: Session, operation: OperationRequest):
+    if not wallets_reposytory.is_wallet_exist(db, operation.wallet_name):
+        raise HTTPException(status_code=404, detail=f"Wallet '{operation.wallet_name}' not found")
+    wallet = wallets_reposytory.get_wallet_balance_by_name(db, operation.wallet_name)
+    if wallet.balance < operation.amount:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Insufficient funds. Available: {wallet.balance}"
+        )
+
+    wallet = wallets_reposytory.add_expense(db, operation.wallet_name, operation.amount)
+    db.commit()
+    return {
+        "message": "Expense added",
+        "wallet": operation.wallet_name,
+        "amount": operation.amount,
+        "description": operation.description,
+        "new_balance": wallet.balance
+    }
--- app/services/wallets.py
+++ app/services/wallets.py
@@ -1,37 +1,29 @@
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
-from app.database import SessionLocal
 from app.schemas import CreateWalletRequest
 from app.repository import wallets as wallets_reposytory
 
 
-def get_wallet(wallet_name: str | None = None):
-    db = SessionLocal()
-    try:
-        if wallet_name is None:
-            wallets = wallets_reposytory.get_all_wallets(db)
-            return {"total_balance": sum([w.amount for w in wallets])}
+def get_wallet(db: Session, wallet_name: str | None = None):
+    if wallet_name is None:
+        wallets = wallets_reposytory.get_all_wallets(db)
+        return {"total_balance": sum([w.balance for w in wallets])}
 
-        if not wallets_reposytory.is_wallet_exist(db, wallet_name):
-            raise HTTPException(status_code=404, detail=f"Wallet '{wallet_name}' not found")
-        wallet = wallets_reposytory.get_wallet_balance_by_name(db, wallet_name)
-        return {"wallet": wallet.name, "balance": wallet.balance}
-    finally:
-        db.close()
-
-def create_wallets(wallet: CreateWalletRequest):
-    db = SessionLocal()
-    try:
-        if wallets_reposytory.is_wallet_exist(db, wallet.name):
-            raise HTTPException(status_code=400, detail=f"Wallet '{wallet.name}' already exists")
+    if not wallets_reposytory.is_wallet_exist(db, wallet_name):
+        raise HTTPException(status_code=404, detail=f"Wallet '{wallet_name}' not found")
+    wallet = wallets_reposytory.get_wallet_balance_by_name(db, wallet_name)
+    return {"wallet": wallet.name, "balance": wallet.balance}
 
 
-        wallet = wallets_reposytory.create_wallet(db, wallet.name, wallet.initial_balance)
-        db.commit()
-        return {
-            "message": f"Wallet '{wallet.name}' created",
-            "wallet": wallet.name,
-            "balance": wallet.balance
-        }
-    finally:
-        db.close()
+def create_wallets(db: Session, wallet: CreateWalletRequest):
+    if wallets_reposytory.is_wallet_exist(db, wallet.name):
+        raise HTTPException(status_code=400, detail=f"Wallet '{wallet.name}' already exists")
+
+    wallet = wallets_reposytory.create_wallet(db, wallet.name, wallet.initial_balance)
+    db.commit()
+    return {
+        "message": f"Wallet '{wallet.name}' created",
+        "wallet": wallet.name,
+        "balance": wallet.balance
+    }
