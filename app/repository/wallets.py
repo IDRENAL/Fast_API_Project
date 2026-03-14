e1f9d991e75c791f5d3a8f2fd6aac8b0546d82f2
Idrenal <dp310793pps@gmail.com>
2026-03-14 12:30:49 +0300

    fix bugs

--- app/repository/wallets.py
+++ app/repository/wallets.py
@@ -1,4 +1,4 @@
-from app.database import SessionLocal
+from decimal import Decimal
 from sqlalchemy.orm import Session
 from app.models import Wallet
 
@@ -7,9 +7,9 @@ def is_wallet_exist(db: Session, wallet_name: str) -> bool:
     return db.query(Wallet).filter(Wallet.name == wallet_name).first() is not None
 
 
-def add_income(db: Session, wallet_name: str, amount: float) -> Wallet:
+def add_income(db: Session, wallet_name: str, amount: Decimal) -> Wallet:
     wallet = db.query(Wallet).filter(Wallet.name == wallet_name).first()
-    wallet.amount += amount
+    wallet.balance += amount
     db.query()
     return wallet
 
@@ -18,9 +18,9 @@ def get_wallet_balance_by_name(db: Session, wallet_name: str) -> Wallet:
     return db.query(Wallet).filter(Wallet.name == wallet_name).first()
 
 
-def add_expense(db: Session, wallet_name: str, amount: float) -> Wallet:
+def add_expense(db: Session, wallet_name: str, amount: Decimal) -> Wallet:
     wallet = db.query(Wallet).filter(Wallet.name == wallet_name).first()
-    wallet.amount -= amount
+    wallet.balance -= amount
     db.query()
     return wallet
 
@@ -29,8 +29,8 @@ def get_all_wallets(db: Session) -> list[Wallet]:
     return db.query(Wallet).all()
 
 
-def create_wallet(db: Session, wallet_name: str, amount: float) -> Wallet:
-    wallet = Wallet(name=wallet_name, amount=amount)
+def create_wallet(db: Session, wallet_name: str, amount: Decimal) -> Wallet:
+    wallet = Wallet(name=wallet_name, balance=amount)
     db.add(wallet)
-    db.refresh(wallet)
+    db.flush()
     return wallet
